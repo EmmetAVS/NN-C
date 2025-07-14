f456cb4db65ea67cad1b574878265fddd7d45e21
Aarush <aarush.shrikanth@gmail.com>
2025-07-13 22:11:08 -0700

    basic layer code

---
+++ include/layer.h
@@ -0,0 +1,22 @@
+#ifndef LAYER_H
+#define LAYER_H
+
+#include "types.h"
+#include "activations.h"
+#include "loss.h"
+
+typedef struct Layer {
+
+    Matrix *weights;
+    Vector *biases;
+    ActivationFunctionForward activation_forward;
+    size_t input_size;
+    size_t output_size;
+
+} Layer;
+
+Layer *create_layer(size_t input_size, size_t output_size, ActivationFunctionForward *activation_forward);
+void destroy_layer(Layer *l);
+Vector *calculate_layer_outputs(Layer *l, Vector *inputs);
+
+#endif
---
+++ src/layer.c
@@ -0,0 +1,42 @@
+#include "layer.h"
+#include "operations.h"
+
+Layer *create_layer(size_t input_size, size_t output_size, ActivationFunctionForward activation_forward) {
+
+    Layer *l = (Layer *)malloc(sizeof(Layer));
+
+    l->activation_forward = activation_forward;
+    l->input_size = input_size;
+    l->output_size = output_size;
+
+    l->biases = create_vector(output_size);
+    l->weights = create_matrix(output_size, input_size);
+
+    return l;
+
+}
+
+void destroy_layer(Layer *l) {
+
+    if (!l)
+        return;
+
+    destroy_matrix(l->weights);
+    destroy_vector(l->biases);
+
+    free(l);
+
+}
+
+Vector *calculate_layer_outputs(Layer *l, Vector *inputs) {
+
+    Vector *logits_no_bias = multiply_matrix_with_vector(l->weights, inputs);
+    Vector *logits = add_vector_to_vector(logits_no_bias, l->biases);
+    destroy_vector(logits_no_bias);
+
+    Vector *activated = (l->activation_forward)(logits);
+    destroy_vector(logits);
+
+    return activated;
+
+}
